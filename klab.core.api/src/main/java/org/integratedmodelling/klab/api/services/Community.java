93b75b14c6e95dc462c15544fff8c6d4f8253023
Ferdinando Villa <ferdinando.villa@gmail.com>
2023-03-22 16:58:15 +0100

    Stub for community service

---
+++ klab.core.api/src/main/java/org/integratedmodelling/klab/api/services/Community.java
@@ -0,0 +1,13 @@
+package org.integratedmodelling.klab.api.services;
+
+/**
+ * The community service handles notifications, messaging, boosting etc. for hub users and for the
+ * hub itself. It directly supports those activity types relevant to k.LAB, in particular to
+ * resource services, such as publishing, reviewing, editing, approving and rejecting.
+ * 
+ * @author Ferd
+ *
+ */
+public interface Community extends KlabService {
+
+}
---
+++ klab.services.community/src/main/java/org/integratedmodelling/klab/services/community/CommunityService.java
@@ -0,0 +1,40 @@
+package org.integratedmodelling.klab.services.community;
+
+import org.integratedmodelling.klab.api.authentication.scope.ServiceScope;
+import org.integratedmodelling.klab.api.services.Community;
+
+public class CommunityService implements Community {
+
+    private static final long serialVersionUID = -7321738633095243998L;
+
+    @Override
+    public ServiceCapabilities getCapabilities() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getUrl() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getLocalName() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getServiceName() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ServiceScope scope() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
