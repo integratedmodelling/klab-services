7fcf1a6b570cf5dd9e8987b8ad73963a606e5efe
Ferdinando Villa <ferdinando.villa@bc3research.org>
2023-08-16 19:09:06 +0200

    Nothing

--- klab.core.api/src/main/java/org/integratedmodelling/klab/api/geometry/Locator.java
+++ klab.core.api/src/main/java/org/integratedmodelling/klab/api/geometry/Locator.java
@@ -1,11 +1,9 @@
 package org.integratedmodelling.klab.api.geometry;
 
-import org.integratedmodelling.klab.api.geometry.impl.OffsetImpl;
-
 /**
- * Locators are topological pointers that can be used to locate and subset observations. A
- * locator is a geometry that comes from a geometry that contains it, and maintains the relationship
- * with the geometry that it locates.
+ * Locators are topological pointers that can be used to locate and subset observations. A locator
+ * is a geometry that comes from a geometry that contains it, and maintains the relationship with
+ * the geometry that it locates.
  * <p>
  * Any geometry or subset of it can be used as a locator. Scales and extents are also locators and
  * can produce their component locators as appropriate. They can also "relocate" by producing lazy
@@ -17,8 +15,8 @@ import org.integratedmodelling.klab.api.geometry.impl.OffsetImpl;
  * Numeric offsets are only exposed to communicate with external raw data APIs; within k.LAB code,
  * translation should happen within the implementing classes, and the "conformant" cases where the
  * locator correspond to a simple offset without mediations should be detected and translated as
- * fast as possible. When offsets are needed, the {@link OffsetImpl} locator can be used as the class in
- * a {@link #as(Class)} request. All locators should implement at least a translation to
+ * fast as possible. When offsets are needed, the {@link OffsetImpl} locator can be used as the
+ * class in a {@link #as(Class)} request. All locators should implement at least a translation to
  * {@link OffsetImpl}.
  * <p>
  * Locators can be parsed from a simple string parameters using the syntax below:
@@ -27,10 +25,10 @@ import org.integratedmodelling.klab.api.geometry.impl.OffsetImpl;
  * &lt;geometry&gt;@n,m,...
  * </pre>
  * 
- * where the {@link Geometry geometry} specs before @ and the @ character itself are optional if
- * the locator is part of a request that unambiguously identifies an observation. Any of the numbers
- * in the list after that is either a linear long integer offset in the correspondent dimension of
- * the geometry or a set of dimensional offsets in parentheses for dimensions with inherent
+ * where the {@link Geometry geometry} specs before @ and the @ character itself are optional if the
+ * locator is part of a request that unambiguously identifies an observation. Any of the numbers in
+ * the list after that is either a linear long integer offset in the correspondent dimension of the
+ * geometry or a set of dimensional offsets in parentheses for dimensions with inherent
  * dimensionality > 1, and each can be substituted by a dot, meaning that the entire dimension is
  * located. If only one offset is mentioned, the remaining ones are substituted with dots. So
  * usually [0] in a temporally explicit context means whatever is located at t=0.
