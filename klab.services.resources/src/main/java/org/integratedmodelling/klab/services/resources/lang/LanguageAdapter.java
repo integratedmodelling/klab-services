cdb22be0226149a7268f9c9d68ecdc6e1a08c392
Ferdinando Villa <ferdinando.villa@gmail.com>
2023-10-30 16:30:24 +0100

    Small changes

--- klab.services.resources/src/main/java/org/integratedmodelling/klab/services/resources/lang/LanguageAdapter.java
+++ klab.services.resources/src/main/java/org/integratedmodelling/klab/services/resources/lang/LanguageAdapter.java
@@ -4,8 +4,8 @@ import org.integratedmodelling.klab.api.lang.impl.kim.KimConceptImpl;
 import org.integratedmodelling.klab.api.lang.impl.kim.KimObservableImpl;
 import org.integratedmodelling.klab.api.lang.kim.KimConcept;
 import org.integratedmodelling.klab.api.lang.kim.KimObservable;
-import org.integratedmodelling.languages.ObservableSyntax;
-import org.integratedmodelling.languages.SemanticsSyntax;
+import org.integratedmodelling.languages.api.ObservableSyntax;
+import org.integratedmodelling.languages.api.SemanticSyntax;
 
 /**
  * Adapter to substitute the current one, based on older k.IM grammars.
@@ -23,7 +23,7 @@ public class LanguageAdapter {
         return ret;
     }
 
-    public KimConcept adaptSemantics(SemanticsSyntax semantics) {
+    public KimConcept adaptSemantics(SemanticSyntax semantics) {
         KimConceptImpl ret = new KimConceptImpl();
         return ret;
     }
