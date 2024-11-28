3e6b03755d9bd321c578d014a9b8c7af355a8e08
Ferdinando Villa <ferdinando.villa@gmail.com>
2024-11-28 16:23:13 +0100

    Notes

--- klab.core.api/src/main/java/org/integratedmodelling/klab/api/services/resolver/objects/ResolutionRequest.java
+++ klab.core.api/src/main/java/org/integratedmodelling/klab/api/services/resolver/objects/ResolutionRequest.java
@@ -10,8 +10,9 @@ import java.util.List;
 /**
  * Request for the runtime to add an observation to the knowledge graph. The resolution constraints come from
  * the scope, creating a new content scope at server side from the root context scope pointed to by the scope
- * token. The observation becomes an observer if the role == OBSERVER, and the knowledge graph has the faculty
- * of providing a default observer if a regular observation is made in a scope that doesn't have one.
+ * token. The observation is forced to be an observer if the role == OBSERVER, which should be used when the
+ * initial observer is submitted into an empty context; otherwise the knowledge graph has the faculty of
+ * providing a default observer if a regular observation is made in a scope that doesn't have one.
  */
 public class ResolutionRequest {
 
