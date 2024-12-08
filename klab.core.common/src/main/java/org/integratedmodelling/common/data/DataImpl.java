c1e669bec7f6e2565001f7c53f19feab7fac5007
Ferdinando Villa <ferdinando.villa@gmail.com>
2024-12-08 21:35:42 +0100

    Avro reader and writer

--- klab.core.common/src/main/java/org/integratedmodelling/common/data/DataImpl.java
+++ klab.core.common/src/main/java/org/integratedmodelling/common/data/DataImpl.java
@@ -1,14 +1,19 @@
 package org.integratedmodelling.common.data;
 
+import org.apache.avro.io.DecoderFactory;
 import org.apache.avro.io.EncoderFactory;
+import org.apache.avro.specific.SpecificDatumReader;
+import org.apache.avro.specific.SpecificDatumWriter;
 import org.integratedmodelling.klab.api.data.Data;
 import org.integratedmodelling.klab.api.data.Metadata;
+import org.integratedmodelling.klab.api.exceptions.KlabIOException;
 import org.integratedmodelling.klab.api.knowledge.Artifact;
 import org.integratedmodelling.klab.api.knowledge.Concept;
 import org.integratedmodelling.klab.api.knowledge.observation.scale.Scale;
 import org.integratedmodelling.klab.api.services.runtime.Notification;
 import org.integratedmodelling.klab.common.data.Instance;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.List;
@@ -23,10 +28,16 @@ import java.util.List;
  */
 public class DataImpl implements Data {
 
-    private Instance instance;
+    private final Instance instance;
 
     public DataImpl(InputStream inputStream) {
-        // TODO
+        try {
+            var decoder = DecoderFactory.get().binaryDecoder(inputStream, null);
+            var reader = new SpecificDatumReader<>(Instance.class);
+            this.instance = reader.read(null, decoder);
+        } catch (IOException e) {
+            throw new KlabIOException(e);
+        }
     }
 
     public DataImpl(Instance instance) {
@@ -39,7 +50,7 @@ public class DataImpl implements Data {
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return instance == null; /* TODO need to check the instance */
     }
 
     @Override
@@ -88,7 +99,14 @@ public class DataImpl implements Data {
     }
 
     public void copyTo(OutputStream dataStream) {
-        var encoder = EncoderFactory.get().binaryEncoder(dataStream, null);
+        try {
+            var encoder = EncoderFactory.get().binaryEncoder(dataStream, null);
+            var writer = new SpecificDatumWriter<>(Instance.class);
+            writer.write(this.instance, encoder);
+            encoder.flush();
+        } catch (IOException e) {
+            throw new KlabIOException(e);
+        }
     }
 
 }
