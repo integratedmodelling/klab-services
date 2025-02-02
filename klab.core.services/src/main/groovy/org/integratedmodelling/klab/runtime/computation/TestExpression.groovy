c0f8d9c0b352792f78076cb1a49c99f20deb2361
Ferdinando Villa <ferdinando.villa@gmail.com>
2025-02-02 21:58:40 +0100

    Examples

--- klab.core.services/src/main/groovy/org/integratedmodelling/klab/runtime/computation/TestExpression.groovy
+++ klab.core.services/src/main/groovy/org/integratedmodelling/klab/runtime/computation/TestExpression.groovy
@@ -4,6 +4,8 @@ import org.integratedmodelling.klab.api.data.Data
 import org.integratedmodelling.klab.api.knowledge.observation.Observation
 import org.integratedmodelling.klab.runtime.storage.DoubleBuffer
 
+// model shit observing elevation, slope
+//  set to [elevation - slope/slope.max]
 class Expression20349 extends ExpressionBase {
 
     @Lazy
@@ -13,13 +15,27 @@ class Expression20349 extends ExpressionBase {
     @Lazy
     DoubleBuffer slope = { scope.digitalTwin.stateStorage.getExistingStorage(observation).buffer(3203, Data.FillCurve.D2_XY, 0) }()
 
-    Expression20349(Observation self, Observation elevation, Observation scope) {
+    Data.FillCurve fillCurve;
+
+    /**
+     * Knows that elevation, slope are qualities and exist
+     *
+     * Calls to known states are translated to _x.data.get(_offset); others to _xObs wrapper
+     *
+     * @param self
+     * @param elevation
+     * @param scope
+     */
+    Expression20349(Observation self, Observation elevation, Observation slope, Observation scope, int offset) {
         this.observationSelf = self;
+//        this._slopeObs = new State(slope)
+
 
     }
 
-    def code() {
+    def code(long offset) {
         // this is the expression set
+        return elevation.data().get(offset) - slope.data().get(offset)/slopeObs.max() // <- NON facile diocan
     }
 
     // TODO this goes upstairs
