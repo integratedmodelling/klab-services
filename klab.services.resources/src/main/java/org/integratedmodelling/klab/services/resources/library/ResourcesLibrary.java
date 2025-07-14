f90c945650f55dc0b6ebdab3a4625202365dec7a
Ferdinando Villa <ferdinando.villa@gmail.com>
2025-07-14 16:54:47 +0200

    Stubs for resource importers

--- klab.services.resources/src/main/java/org/integratedmodelling/klab/services/resources/library/ResourcesLibrary.java
+++ klab.services.resources/src/main/java/org/integratedmodelling/klab/services/resources/library/ResourcesLibrary.java
@@ -1,5 +1,6 @@
 package org.integratedmodelling.klab.services.resources.library;
 
+import org.integratedmodelling.common.utils.Utils;
 import org.integratedmodelling.klab.api.collections.Parameters;
 import org.integratedmodelling.klab.api.data.Version;
 import org.integratedmodelling.klab.api.knowledge.Artifact;
@@ -13,6 +14,7 @@ import org.integratedmodelling.klab.services.base.BaseService;
 import org.integratedmodelling.klab.services.resources.ResourcesProvider;
 
 import java.io.File;
+import java.util.Map;
 
 @Library(
     name = "resources",
@@ -26,7 +28,8 @@ public class ResourcesLibrary {
       description = "Register a new resource by importing a legacy json manifest",
       mediaType = "application/json",
       fileExtensions = {"json"})
-  public static String importLegacyResourceJson(File archive, ResourcesProvider service, UserScope scope) {
+  public static String importLegacyResourceJson(
+      File archive, ResourcesProvider service, UserScope scope) {
     return null;
   }
 
@@ -36,27 +39,43 @@ public class ResourcesLibrary {
       description = "Register a new resource by importing a zip file with legacy content",
       mediaType = "application/zip",
       fileExtensions = {"zip"})
-  public static String importLegacyResourceZip(File archive, ResourcesProvider service, UserScope scope) {
+  public static String importLegacyResourceZip(
+      File archive, ResourcesProvider service, UserScope scope) {
     return null;
   }
 
   @Importer(
-          schema = "json",
-          knowledgeClass = KlabAsset.KnowledgeClass.PROJECT,
-          description = "Register a new resource without content by importing a compliant json manifest",
-          mediaType = "application/json",
-          fileExtensions = {"json"})
-  public static String importResourceJson(File archive, ResourcesProvider service, UserScope scope) {
+      schema = "json",
+      knowledgeClass = KlabAsset.KnowledgeClass.PROJECT,
+      description =
+          "Register a new resource without content by importing a compliant json manifest",
+      mediaType = "application/json",
+      fileExtensions = {"json"})
+  public static String importResourceJson(
+      File archive, ResourcesProvider service, UserScope scope) {
+    var definition = Utils.Json.load(archive, Map.class);
+    if (definition != null) {
+      return loadLegacyResource(definition, service, scope);
+    }
     return null;
   }
 
   @Importer(
-          schema = "zip",
-          knowledgeClass = KlabAsset.KnowledgeClass.PROJECT,
-          description = "Register a new resource by importing a zip file with full content, including the json manifest",
-          mediaType = "application/zip",
-          fileExtensions = {"zip"})
+      schema = "zip",
+      knowledgeClass = KlabAsset.KnowledgeClass.PROJECT,
+      description =
+          "Register a new resource by importing a zip file with full content, including the json manifest",
+      mediaType = "application/zip",
+      fileExtensions = {"zip"})
   public static String importResourceZip(File archive, ResourcesProvider service, UserScope scope) {
     return null;
   }
+
+
+  private static String loadLegacyResource(
+          Map definition, ResourcesProvider service, UserScope scope) {
+    return null;
+  }
+
+
 }
