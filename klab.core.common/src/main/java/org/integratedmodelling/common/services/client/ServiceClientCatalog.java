a06622e87cf30e052064cb04686df28e0b33f302
Ferdinando Villa <ferdinando.villa@bc3research.org>
2025-10-02 18:54:01 +0200

    Move on with refactoring

--- klab.core.common/src/main/java/org/integratedmodelling/common/services/client/ServiceClientCatalog.java
+++ klab.core.common/src/main/java/org/integratedmodelling/common/services/client/ServiceClientCatalog.java
@@ -17,6 +17,7 @@ import org.integratedmodelling.klab.api.exceptions.KlabIllegalStateException;
 import org.integratedmodelling.klab.api.identities.UserIdentity;
 import org.integratedmodelling.klab.api.scope.UserScope;
 import org.integratedmodelling.klab.api.services.KlabService;
+import org.integratedmodelling.klab.api.services.impl.ServiceStatusImpl;
 import org.integratedmodelling.klab.api.services.runtime.Channel;
 import org.integratedmodelling.klab.api.services.runtime.objects.UserScopeNotification;
 
@@ -47,6 +48,7 @@ public enum ServiceClientCatalog {
     private final KlabService.Type type;
     private final AtomicReference<KlabService.ServiceStatus> status;
     private final boolean local;
+    private ScheduledFuture<?> schedule;
 
     public Utils.Http.Client getClient() {
       return client;
@@ -90,54 +92,108 @@ public enum ServiceClientCatalog {
     }
 
     void connect() {
-      //      var secret = Configuration.INSTANCE.getServiceSecret(type);
-      //      if (secret != null) {
-      //        local = Utils.URLs.isLocalHost(getUrl());
-      //        if (local) {
-      //          client.setHeader(ServicesAPI.SERVER_KEY_HEADER, secret);
-      //          ret = secret;
-      //        }
-      //      }
-      //
-      //      if (statusListeners != null) {
-      //        this.statusListeners.addAll(Arrays.asList(statusListeners));
-      //      }
-
-      //      this.scope =
-      //          new AbstractServiceDelegatingScope(channel) {
-      //
-      //            @Override
-      //            public UserScope createUser(String username, String password) {
-      //              return null;
-      //            }
-      //
-      //            @Override
-      //            public <T extends KlabService> T getService(
-      //                Class<T> serviceClass, Predicate<T>... selectors) {
-      //              return KlabService.Type.classify(serviceClass) == serviceType
-      //                  ? (T) ServiceClient.this
-      //                  : null;
-      //            }
-      //
-      //            @Override
-      //            public <T extends KlabService> Collection<T> getServices(Class<T> serviceClass)
-      // {
-      //              return KlabService.Type.classify(serviceClass) == serviceType
-      //                  ? (Collection<T>) List.of(ServiceClient.this)
-      //                  : Collections.emptyList();
-      //            }
-      //          };
-
-      pollingTasks.scheduleAtFixedRate(
-          this::timedTasks,
-          0,
-          this.local ? localPollCycleSeconds : onlinePollCycleSeconds,
-          TimeUnit.SECONDS);
+      this.schedule =
+          pollingTasks.scheduleAtFixedRate(
+              this::timedTasks,
+              0,
+              this.local ? localPollCycleSeconds : onlinePollCycleSeconds,
+              TimeUnit.SECONDS);
     }
 
-    void timedTasks() {}
+    void timedTasks() {
 
-    void close() {}
+      //        if (settings != null && "off".equals(settings.get(Setting.POLLING, String.class))) {
+      //            return;
+      //        }
+      //
+      //        //    if (this.shutdown.get()) {
+      //        //      //      scope.send(
+      //        //      //          Message.MessageClass.ServiceLifecycle,
+      //        //      //          Message.MessageType.ServiceStatus,
+      //        //      //          ServiceStatus.offline(serviceType, this.serviceId()));
+      //        //      return;
+      //        //    }
+      //
+      //        try {
+      //
+      //            //      var connectedBeforeChecking = connected.get();
+      //            var statusBeforeChecking = status.get();
+      //
+      //      /*
+      //      TODO check for changes of status and send messages over
+      //       */
+      //            try {
+      //                var currentServiceStatus = readServiceStatus(this.url, scope);
+      //                if (currentServiceStatus == null) {
+      //                    connected.set(false);
+      //                    shutdown.set(false);
+      //                    status.set(
+      //                            KlabService.ServiceStatus.offline(
+      //                                    serviceType,
+      //                                    this.capabilities == null ? null :
+      // this.capabilities.getServiceId()));
+      //                } else {
+      //                    status.set(currentServiceStatus);
+      //                    connected.set(true);
+      //          /*
+      //          System.out.println("Service " + currentServiceStatus.getServiceType()
+      //                  + " with id "+currentServiceStatus.getServiceId() + " is "
+      //                  + ((currentServiceStatus.isAvailable()) ? "online" : "offline"));
+      //
+      //           */
+      //                    if (this.capabilities == null) {
+      //                        this.capabilities = capabilities(scope);
+      //                        if (this.capabilities != null) {
+      //                            this.serviceId = capabilities.getServiceId();
+      //                        }
+      //                    }
+      //                }
+      //
+      //                ((ServiceStatusImpl) status.get()).setShutdown(this.shutdown.get());
+      //
+      //            } finally {
+      //
+      //                boolean statusHasChanged =
+      //                        (statusBeforeChecking == null && status.get() != null)
+      //                                || (statusBeforeChecking != null && status.get() == null)
+      //                                || (status.get() != null
+      //                                && statusBeforeChecking != null
+      //                                && status.get().hasChangedComparedTo(statusBeforeChecking));
+      //
+      //                if (connected.get()) {
+      //
+      //                    // see if we have a local service and change the token
+      //                    if ((token == null || token.isEmpty()) &&
+      // Utils.URLs.isLocalHost(getUrl())) {
+      //                        // may have gotten lost if the service was starting when we booted
+      //                        var secret = Configuration.INSTANCE.getServiceSecret(serviceType);
+      //                        if (secret != null) {
+      //                            token = secret;
+      //                            client.setAuthorization(token);
+      //                            local = true;
+      //                        }
+      //                    }
+      //                }
+      //
+      //                if (statusHasChanged) {
+      //                    this.capabilities = capabilities(scope);
+      //                }
+      //
+      //                for (var listener : statusListeners) {
+      //                    listener.accept(status.get(), statusHasChanged);
+      //                }
+      //            }
+      //
+      //        } catch (Throwable t) {
+      //            scope.error(t);
+      //        }
+      //
+      //        this.connectionAttempted.set(true);
+    }
+
+    void close() {
+      this.schedule.cancel(true);
+    }
   }
 
   private final Map<String, ServiceMonitor> serviceClients = new ConcurrentHashMap<>();
