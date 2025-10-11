6a7cab006e649b3d98498b54c8821eba1d9fb053
Ferdinando Villa <ferdinando.villa@bc3research.org>
2025-10-11 19:02:50 +0200

    Notes

--- klab.core.services/src/main/java/org/integratedmodelling/klab/runtime/kactors/compiler/ActorExample.java
+++ klab.core.services/src/main/java/org/integratedmodelling/klab/runtime/kactors/compiler/ActorExample.java
@@ -5,15 +5,26 @@ import org.integratedmodelling.klab.api.lang.kactors.KActorsBehavior;
 ///  # TO BE REMOVED - example translation of following k.Actors code:
 ///
 ///
+/// ```
+/// action main:
+///    emitter: sentence -> console.print(["Emitter said " + sentence])
+///    functor: x -> console.print(x)
 ///
+/// // a functor
+/// action functor:
+///     console.print("Dio porco ho bestemmiato, dio can")
+/// 	return "dio can"
 ///
-///
+///  // an emitter (fires periodically, no return, continues firing)
+/// action emitter:
+///         timer.random(step=10.s): time -> fire "dio puto"
+/// ```
 ///
 /// This must be created by the actor compiler and compiled to a .class internally.
 ///
 public class ActorExample extends ActorBase {
 
-    public ActorExample(KActorsBehavior behavior) {
-        super(behavior);
-    }
+  public ActorExample(KActorsBehavior behavior) {
+    super(behavior);
+  }
 }
