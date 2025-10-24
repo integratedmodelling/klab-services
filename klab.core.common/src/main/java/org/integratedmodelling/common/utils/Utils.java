aa2a292a6d912d761d3ed550f4ea6dbc0ef695a8
Enrico Girotto <enrico.girotto@gmail.com>
2025-10-24 13:51:12 +0200

    Solve issue with capabilities deserialize
    The empties list was returned as an empty array, throwing an exception.

--- klab.core.common/src/main/java/org/integratedmodelling/common/utils/Utils.java
+++ klab.core.common/src/main/java/org/integratedmodelling/common/utils/Utils.java
@@ -1831,7 +1831,11 @@ public class Utils extends org.integratedmodelling.klab.api.utils.Utils {
             throw new KlabIllegalArgumentException(e);
           }
         }
-        return Json.parseObject(body, resultClass);
+        try {
+            return Utils.parseWithJackson(body, resultClass);
+        } catch (IOException e) {
+            throw new KlabIOException(e);
+        }
       }
 
       @Override
@@ -3607,5 +3611,19 @@ public class Utils extends org.integratedmodelling.klab.api.utils.Utils {
     public File getFile() {
       return file;
     }
+  }
+
+  /**
+   * Add an option to avoid problem if Lists are serialized as empty arryas
+   * @param body the body
+   * @param resultClass the resutl class
+   * @return
+   * @param <T> Parsed object
+   * @throws IOException
+   */
+  public static <T> T parseWithJackson(String body, Class<T> resultClass) throws IOException {
+    ObjectMapper mapper = new ObjectMapper();
+    mapper.configure(DeserializationFeature.ACCEPT_EMPTY_ARRAY_AS_NULL_OBJECT, true);
+    return mapper.readValue(body, resultClass);
   }
 }
