10b738b463e9520e0ef8f632aa78a12f7fe37132
Ferdinando Villa <ferdinando.villa@gmail.com>
2025-11-25 10:23:26 +0100

    Restore essential services (temporarily)

--- klab.services.runtime.server/src/main/java/org/integratedmodelling/klab/services/runtime/server/RuntimeServer.java
+++ klab.services.runtime.server/src/main/java/org/integratedmodelling/klab/services/runtime/server/RuntimeServer.java
@@ -1,6 +1,5 @@
 package org.integratedmodelling.klab.services.runtime.server;
 
-
 import org.integratedmodelling.common.authentication.scope.AbstractServiceDelegatingScope;
 import org.integratedmodelling.klab.api.services.KlabService;
 import org.integratedmodelling.common.services.ServiceStartupOptions;
@@ -14,44 +13,49 @@ import java.util.List;
 
 @Component
 // TODO remove the argument when all gson dependencies are the same (never)
-@EnableAutoConfiguration(exclude = {org.springframework.boot.autoconfigure.gson.GsonAutoConfiguration.class})
-@ComponentScan(basePackages = {"org.integratedmodelling.klab.services.application.security",
-                               "org.integratedmodelling.klab.services.messaging",
-                               "org.integratedmodelling.klab.services.application.controllers",
-                               "org.integratedmodelling.klab.services.runtime.server.controllers"})
+@EnableAutoConfiguration(
+    exclude = {org.springframework.boot.autoconfigure.gson.GsonAutoConfiguration.class})
+@ComponentScan(
+    basePackages = {
+      "org.integratedmodelling.klab.services.application.security",
+      "org.integratedmodelling.klab.services.messaging",
+      "org.integratedmodelling.klab.services.application.controllers",
+      "org.integratedmodelling.klab.services.runtime.server.controllers"
+    })
 public class RuntimeServer extends ServiceNetworkedInstance<RuntimeService> {
 
-    @Override
-    protected KlabService.Type serviceType() {
-        return KlabService.Type.RUNTIME;
-    }
+  @Override
+  protected KlabService.Type serviceType() {
+    return KlabService.Type.RUNTIME;
+  }
 
-    @Override
-    protected List<KlabService.Type> getEssentialServices() {
-        /**
-         * This runtime gets resolvers and resource services from the observation requests, so does not need
-         * its own services besides reasoning.
-         *
-         * TODO the context request contains the service URLs actually. So all these should be optional only
-         *  for the runtime to work standalone (but there are issues if these aren't there).
-         */
-        return List.of(/*KlabService.Type.RESOURCES, KlabService.Type.REASONER, KlabService.Type.RESOLVER*/);
-    }
+  @Override
+  protected List<KlabService.Type> getEssentialServices() {
+    /**
+     * This runtime gets resolvers and resource services from the observation requests, so does not
+     * need its own services besides reasoning.
+     *
+     * <p>TODO the context request contains the service URLs actually. So all these should be
+     * optional only for the runtime to work standalone (but there are issues if these aren't
+     * there).
+     */
+    return List.of(
+        KlabService.Type.RESOURCES, KlabService.Type.REASONER, KlabService.Type.RESOLVER);
+  }
 
-    @Override
-    protected List<KlabService.Type> getOperationalServices() {
-        return List.of();
-    }
+  @Override
+  protected List<KlabService.Type> getOperationalServices() {
+    return List.of();
+  }
 
-    @Override
-    protected RuntimeService createPrimaryService(AbstractServiceDelegatingScope serviceScope,
-                                                  ServiceStartupOptions options) {
-        return new RuntimeService(serviceScope, options);
-    }
+  @Override
+  protected RuntimeService createPrimaryService(
+      AbstractServiceDelegatingScope serviceScope, ServiceStartupOptions options) {
+    return new RuntimeService(serviceScope, options);
+  }
 
-    public static void main(String[] args) {
-        ServiceNetworkedInstance.start(RuntimeServer.class,
-                ServiceStartupOptions.create(KlabService.Type.RUNTIME, args));
-    }
-
+  public static void main(String[] args) {
+    ServiceNetworkedInstance.start(
+        RuntimeServer.class, ServiceStartupOptions.create(KlabService.Type.RUNTIME, args));
+  }
 }
