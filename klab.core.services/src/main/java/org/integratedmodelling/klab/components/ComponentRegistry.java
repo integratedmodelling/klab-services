e57ecb845f296b794988550f7b2fe15ccb3c853b
Ferdinando Villa <ferdinando.villa@bc3research.org>
2025-12-21 12:36:48 +0100

    Comment out Iñigo's Christmas gift so I can use components again

--- klab.core.services/src/main/java/org/integratedmodelling/klab/components/ComponentRegistry.java
+++ klab.core.services/src/main/java/org/integratedmodelling/klab/components/ComponentRegistry.java
@@ -1408,7 +1408,8 @@ public class ComponentRegistry {
    * @param pluginRoot
    */
   public void initializeComponents(File pluginRoot) {
-    this.componentManager = new KlabPluginManager(pluginRoot.toPath());
+    this.componentManager = new DefaultPluginManager(pluginRoot.toPath());
+    //    this.componentManager = new KlabPluginManager(pluginRoot.toPath());
     this.componentManager.loadPlugins();
     this.pluginPath = pluginRoot;
     // TODO configuration
