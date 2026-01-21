1bd5790850b3a0cab876f678c5ced29e5b776a34
Andrea Antonello <andrea.antonello@gmail.com>
2026-01-21 09:32:50 +0100

    first set of migrations towards geotools 34 - set back h2gis to supported version 1.5.0

--- klab.core.services/src/main/java/org/integratedmodelling/klab/runtime/scale/space/ProjectionImpl.java
+++ klab.core.services/src/main/java/org/integratedmodelling/klab/runtime/scale/space/ProjectionImpl.java
@@ -1,16 +1,17 @@
 package org.integratedmodelling.klab.runtime.scale.space;
 
+import java.util.Objects;
+
+import javax.measure.Unit;
+
+import org.geotools.api.referencing.FactoryException;
+import org.geotools.api.referencing.crs.CoordinateReferenceSystem;
 import org.geotools.referencing.CRS;
 import org.geotools.referencing.CRS.AxisOrder;
 import org.integratedmodelling.klab.api.exceptions.KlabIllegalStateException;
 import org.integratedmodelling.klab.api.exceptions.KlabValidationException;
 import org.integratedmodelling.klab.api.knowledge.observation.scale.space.Envelope;
 import org.integratedmodelling.klab.api.knowledge.observation.scale.space.Projection;
-import org.opengis.referencing.FactoryException;
-import org.opengis.referencing.crs.CoordinateReferenceSystem;
-
-import javax.measure.Unit;
-import java.util.Objects;
 
 public class ProjectionImpl implements Projection {
 
