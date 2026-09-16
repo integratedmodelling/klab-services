bce95e54b87c86ed7be82e5134738f7971d7bdf1
Enrico Girotto <enrico.girotto@bc3research.org>
2026-09-16 11:51:18 +0200

    Add icons

--- klab.core.services/src/main/webui/src/main.ts
+++ klab.core.services/src/main/webui/src/main.ts
@@ -1,12 +1,21 @@
 import { createApp } from "vue";
 import { Notify, Quasar } from "quasar";
+
+import "@quasar/extras/material-icons/material-icons.css";
 import "quasar/dist/quasar.css";
+
 import App from "./App.vue";
 import "./styles.css";
 
 createApp(App)
   .use(Quasar, {
     plugins: { Notify },
-    config: { brand: { primary: "#18b795", secondary: "#b9f36b", dark: "#092f2a" } },
+    config: {
+      brand: {
+        primary: "#18b795",
+        secondary: "#b9f36b",
+        dark: "#092f2a",
+      },
+    },
   })
   .mount("#app");
